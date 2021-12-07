9f7be6a4e230ce12dac1415908edd00cfb4949d4
Roland <roland1105@live.com>
2021-12-07 17:31:00 -0400

    Commit inicial

---
+++ Backend/app/config/db.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  HOST: "localhost",
+  PORT: 27017,
+  DB: "bezkoder_db",
+  URI: "mongodb+srv://musicgeo:YYujsju64h2Ecxxs@cluster0.9s22a.mongodb.net/MusicanGeovanyDB?retryWrites=true&w=majority"
+};
